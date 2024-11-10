49780854935a2b3b909aa91006f52a1be033d9f7
MohamedBarbych <MohamedBarbych@users.noreply.github.com>
2024-11-10 16:25:51 +0100

    Remove sensitive data from package.json

--- src/main/java/com/gsb/dao/entities/Client.java
+++ src/main/java/com/gsb/dao/entities/Client.java
@@ -1,5 +1,4 @@
 package com.gsb.dao.entities;
-
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
